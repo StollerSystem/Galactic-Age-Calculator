d3ba317faa95c3fd357c2ed293a7cd73b9a4fb8d
Ben Stoller <stollersystemsound@gmail.com>
2020-09-04 09:12:12 -0700

    write test for spec 4

--- __tests__/galactic_age.test.js
+++ __tests__/galactic_age.test.js
@@ -2,21 +2,26 @@ import GalacticAge from '../src/js/galactic_age.js';
 
 describe('Galactic Age', () => {
     let galacticAge;
+
     beforeEach(() => {
       galacticAge = new GalacticAge(50);
     });  
 
   test('1 should return users age in Mercury years', () => {
       expect(galacticAge.mercury()).toEqual(208)
-  });
+    });
 
   test('2 should return users age in Venus years', () => {
       expect(galacticAge.venus()).toEqual(80)
-  });
+    });
 
   test('3 should return users age in Mars years', () => {
-    expect(galacticAge.mars()).toEqual(26)
-});
+      expect(galacticAge.mars()).toEqual(26)
+    });
+
+  test('4 should return users age in Jupiter years', () => {
+      expect(galacticAge.mars()).toEqual(26)
+    });
 
 
 
