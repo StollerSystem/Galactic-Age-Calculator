35e6da2d805070e7d60244f5c41cabe645185256
Ben Stoller <stollersystemsound@gmail.com>
2020-09-04 11:56:25 -0700

    add alert if age input is not correct

--- src/js/galactic_age.js
+++ src/js/galactic_age.js
@@ -1,39 +1,39 @@
 export default class GalacticAge {
   constructor(userAge){
-    this.userAge = userAge
+    this.userAge = userAge;
   }
 
   mercury() {    
-    return parseInt((this.userAge*365)/(365*.24))
+    return parseInt((this.userAge*365)/(365*.24));
   }
 
   venus() {
-    return parseInt((this.userAge*365)/(365*.62))
+    return parseInt((this.userAge*365)/(365*.62));
   }
 
   mars() {
-    return parseInt((this.userAge*365)/(365*1.88 ))
+    return parseInt((this.userAge*365)/(365*1.88 ));
   }
 
   jupiter() {
-    return parseInt((this.userAge*365)/(365*11.86 ))
+    return parseInt((this.userAge*365)/(365*11.86 ));
   }
 
   yearsLeft(planet,ageExpectancy) {
     let yearsLeft; 
     switch (planet) {
-      case ("mercury"):
-        yearsLeft = parseInt((ageExpectancy*365)/(365*.24)-(this.userAge*365)/(365*.24));
-        break;        
-      case ("venus"):
-        yearsLeft = parseInt((ageExpectancy*365)/(365*.62)-(this.userAge*365)/(365*.62));   
-        break;     
-      case ("mars"):
-        yearsLeft = parseInt((ageExpectancy*365)/(365*1.88)-(this.userAge*365)/(365*1.88));   
-        break;     
-      case ("jupiter"):
-        yearsLeft = parseFloat(((ageExpectancy*365)/(365*11.86)-(this.userAge*365)/(365*11.86)).toFixed(2));    
-        break;
+    case ("mercury"):
+      yearsLeft = parseInt((ageExpectancy*365)/(365*.24)-(this.userAge*365)/(365*.24));
+      break;        
+    case ("venus"):
+      yearsLeft = parseInt((ageExpectancy*365)/(365*.62)-(this.userAge*365)/(365*.62));   
+      break;     
+    case ("mars"):
+      yearsLeft = parseInt((ageExpectancy*365)/(365*1.88)-(this.userAge*365)/(365*1.88));   
+      break;     
+    case ("jupiter"):
+      yearsLeft = parseFloat(((ageExpectancy*365)/(365*11.86)-(this.userAge*365)/(365*11.86)).toFixed(2));    
+      break;
     }
     
     if (this.userAge > ageExpectancy) {
--- src/main.js
+++ src/main.js
@@ -5,22 +5,20 @@ import './css/styles.css';
 import GalacticAge from './js/galactic_age.js';
 
 
-let galacticAge = new GalacticAge(0)
+let galacticAge = new GalacticAge(0);
 $("#galactic-age-form").submit(function(event) {
-  event.preventDefault();
-  galacticAge.userAge = parseInt($("#ageInput").val())
-  //console.log(userAge)
-  //let galacticAge = new GalacticAge(userAge)
-  //console.log(galacticAge.userAge)
-  //console.log(galacticAge.mercury())
-  //$("#output").append(`<li> Your age on Mercury: ${galacticAge.mercury().toString()}</li>`)
-  displayAge($("#planet").val())
-
+  event.preventDefault();  
+  if (!($("#ageInput").val() > 0)) {
+    alert("Please enter your age! (in numbers)");
+  } else {
+    galacticAge.userAge = parseInt($("#ageInput").val());
+    displayAge($("#planet").val());
+  }
 
 });
 
 function displayAge (planet) {
-  $("#output").append(`<li> Your age on Mercury: ${galacticAge[planet]().toString()}</li>`)
+  $("#output").append(`<li> Your age on ${planet}: ${galacticAge[planet]().toString()}</li>`);
 }
 
 
