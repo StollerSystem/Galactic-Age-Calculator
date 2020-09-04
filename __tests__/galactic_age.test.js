7134ab2842c0263349d38d233059d0b16b316047
Ben Stoller <stollersystemsound@gmail.com>
2020-09-04 08:44:01 -0700

    write test for spec 1

--- __tests__/galactic_age.test.js
+++ __tests__/galactic_age.test.js
@@ -1,9 +1,14 @@
 import GalacticAge from '../src/js/rectangle.js';
 
 describe('Galactic Age', () => {
+    let galacticAge;
+    beforeEach(() => {
+      galacticAge = new GalacticAge();
+    });
+  
 
-  test('', () => {
-
+  test('should return users age in Mercury years', () => {
+      expect(GalacticAge.mercury(50)).toEqual(207)
   });
 
 
